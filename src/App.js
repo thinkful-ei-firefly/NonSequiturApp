d7b7fa685799e8427100eb640348571c4a7f4be6
Kiami Nichols <kiamimariah@yahoo.com>
2019-09-11 11:30:18 -0400

    splash add, checked boxes in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Splash from './Splash';
+
 
 function App() {
   return (
     <main className='App'>
-      { <p> NonSequitur Application </p>}
+       <p> NonSequitur Application </p>
+       <Splash></Splash>
     </main>
   );
 }
 
+
+
+/* this.state = {
+  splash: true
+};
+ */
 export default App;
--- src/splash.js
+++ src/splash.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Logo512 from './logo512.png';
+import logo512 from './logo512.png';
 
 export default class Splash extends Component {
     render() {
         return(
             <div id='Splashwrapper'>
-                <img src={Logo512}></img>
+                <img src={logo512} alt='reactlogo'></img>
             </div>
         );
     }
